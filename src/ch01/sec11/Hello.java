96c7fc047c960b6ebe30a0c984631f5a6627072b
soheeG <121710859+soheeG@users.noreply.github.com>
2023-02-14 10:54:39 +0900

    first commit

---
+++ src/ch01/sec11/Hello.java
@@ -0,0 +1,25 @@
+package ch01.sec11;
+/**
+ * @author user
+ */
+/*
+ 장제목 : 1장 자바 시작하기
+ 작성일 : 2023.02.14
+ */
+public class Hello {
+	// 프로그램 실행 진입점
+	public static void main(String[] args) {
+		// 한 줄 주석
+		/*
+		 * 여러 줄 주석
+		 * 여러 줄 주석
+		 * 여러 줄 주석
+		 */
+		/** 
+		 * 도큐먼트 주석 : 클래스 위나 메소드 위 문서화 되는 주석 
+		 */
+		// 단축키 : ctrl + /
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello World");
+	}
+}
---
+++ src/ch01/sec11/MainShortCut.java
@@ -0,0 +1,8 @@
+package ch01.sec11;
+
+public class MainShortCut {
+	public static void main(String[] args) {
+		System.out.println("Hi JAVA")
+		System.out.println();
+	}
+}
