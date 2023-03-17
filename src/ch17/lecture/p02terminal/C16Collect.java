ae7ad8c46fe6f5eb2254e3b458c22ce94900eda1
soheeG <121710859+soheeG@users.noreply.github.com>
2023-03-17 16:59:21 +0900

    20230317

--- src/ch17/lecture/p02terminal/C16Collect.java
+++ src/ch17/lecture/p02terminal/C16Collect.java
@@ -3,7 +3,7 @@ package ch17.lecture.p02terminal;
 import java.util.*;
 import java.util.stream.*;
 
-public class C6Collect {
+public class C16Collect {
 	public static void main(String[] args) {
 		List<Integer> list = List.of(3, 2, 3, 2, 2, 2);
 		
