65cc9913345e9347808dcd08cf90255625859c67
soheeG <121710859+soheeG@users.noreply.github.com>
2023-03-27 15:01:57 +0900

    20230327

--- src/ch12/lecture/p06annotation/C06Annotation.java
+++ src/ch12/lecture/p06annotation/C06Annotation.java
@@ -3,3 +3,19 @@ package ch12.lecture.p06annotation;
 public class C06Annotation {
 
 }
+
+class MyClass06 {
+	@MyAnnotaion06
+	String field1;
+	
+	@MyAnnotaion06(myAttr = 10)
+	int field2;
+	
+	@MyAnnotaion06
+	boolean field3;
+}
+
+@interface MyAnnotaion06 {
+	int myAttr() default 0;
+	
+}
